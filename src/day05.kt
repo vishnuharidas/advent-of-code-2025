481b82795b3265389d77fc72aac3dd38fc61c534
Vishnu Haridas <hvishnu999@gmail.com>
2025-12-05 17:03:51 +0400

    Day 05 Part 2

--- src/day05.kt
+++ src/day05.kt
@@ -21,5 +21,32 @@ fun main() {
             ranges.any { range -> id in range }
         }
 
+    fun mergeRanges(ranges: List<ULongRange>): List<ULongRange> {
+        if (ranges.isEmpty()) return emptyList()
+
+        val sortedRanges = ranges.sortedBy { it.first }
+        val mergedRanges = mutableListOf<ULongRange>()
+        var currentRange = sortedRanges[0]
+
+        for (i in 1 until sortedRanges.size) {
+            val nextRange = sortedRanges[i]
+            if (currentRange.last + 1u >= nextRange.first) {
+                currentRange = currentRange.first..maxOf(currentRange.last, nextRange.last)
+            } else {
+                mergedRanges.add(currentRange)
+                currentRange = nextRange
+            }
+        }
+
+        mergedRanges.add(currentRange)
+
+        return mergedRanges
+    }
+
+    fun part2(ranges: List<ULongRange>) =
+        mergeRanges(ranges)
+            .sumOf { it.last - it.first + 1u }
+
     println("Part 1: ${part1(availableIds, ranges)}")
+    println("Part 2: ${part2(ranges)}")
 }
