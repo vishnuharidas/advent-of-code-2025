fcc5ee5e7ca0f6cdb8d34b285635b38818351477
Vishnu Haridas <hvishnu999@gmail.com>
2025-12-07 01:32:40 +0400

    Day 06 Part 2

--- src/day06.kt
+++ src/day06.kt
@@ -4,16 +4,17 @@ fun main() {
 
     val input = R.getContents("day06/input.txt")
 
-    val numbersList = input.dropLast(1)
-        .map {
-            it.split(" ")
-        }
 
-    val opsList = input.last()
-        .split("")
-        .filter { it.isNotBlank() }
+    fun part1(input: List<String>): Long {
 
-    fun part1(numbersList: List<List<String>>, opsList: List<String>): Long {
+        val numbersList = input.dropLast(1)
+            .map {
+                it.split(" ")
+            }
+
+        val opsList = input.last()
+            .split("")
+            .filter { it.isNotBlank() }
 
         // Convert strings to longs, ignoring blank strings
         val numbers = numbersList.map { row ->
@@ -41,6 +42,51 @@ fun main() {
         return resultsList.sum()
     }
 
+    fun part2(input: List<String>): Long {
 
-    println("Part 1: ${part1(numbersList, opsList)}")
+        val numbersList = input.dropLast(1)
+
+        val opsList = input.last()
+            .split("")
+            .filter { it.isNotBlank() }
+
+        // Arrange numbers by their position
+        val maxLength = numbersList.maxOfOrNull { it.length } ?: 0
+        val arrangedNumsList = (0..maxLength)
+            .map {
+                numbersList.map { str -> str.getOrNull(it) ?: ' ' }
+                    .joinToString("")
+                    .trim()
+            }
+
+        // Split into groups, considering blanks as separators
+        val groupsList = mutableListOf<List<Long>>()
+        var tempList = mutableListOf<Long>()
+
+        for (i in arrangedNumsList) {
+            val n = i.toLongOrNull()
+            if (n != null) {
+                tempList.add(n)
+            } else {
+                groupsList.add(tempList)
+                tempList = mutableListOf()
+            }
+        }
+
+        return groupsList.mapIndexed { rowIndex, row ->
+
+            val result = when (val op = opsList[rowIndex]) {
+                "+" -> row.sum()
+                "*" -> row.reduce { acc, num -> acc * num }
+                else -> throw IllegalArgumentException("Unknown operation: $op")
+            }
+            result
+
+        }.sum()
+    }
+
+
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
