8c3c7a0b630e1df6b9d3ed01eb1e9892077add06
Vishnu Haridas <hvishnu999@gmail.com>
2025-12-09 20:04:53 +0400

    Day 07 Part 02

--- src/day07.kt
+++ src/day07.kt
@@ -41,6 +41,54 @@ fun main() {
         return splitCount
     }
 
+    fun part2(input: List<String>): Long {
+
+        fun countFinishes(
+            grid: List<String>,
+            row: Int,
+            col: Int,
+            visited: MutableMap<Pair<Int, Int>, Long>
+        ): Long {
+            // reached past bottom -> one successful finish
+            if (row >= grid.size) {
+                return 1L
+            }
+
+            // if already visited, return cached result
+            if (visited[Pair(row, col)] != null) {
+                return visited[Pair(row, col)]!!
+            }
+
+            val result = when (grid[row][col]) {
+                '^' -> {
+                    // go left and right from this row/col
+                    countFinishes(grid, row, col - 1, visited) + countFinishes(grid, row, col + 1, visited)
+                }
+
+                '.', 'S' -> {
+                    // go straight down
+                    countFinishes(grid, row + 1, col, visited)
+                }
+
+                else -> 0L
+            }
+
+            // cache result - to avoid recomputation every time a ray passes through this point again in the future.
+            visited[Pair(row, col)] = result
+
+            return result
+        }
+
+
+        val firstPosition = input.first().indexOf("S")
+        val visited = mutableMapOf<Pair<Int, Int>, Long>()
+
+        val finishCount = countFinishes(input, 0, firstPosition, visited)
+
+        return finishCount
+    }
+
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 
 }
